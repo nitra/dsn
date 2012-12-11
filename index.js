4f7704799ae2b407b785d38dd9e26734ae630ece
Aria Stewart <aredridel@nbtsc.org>
2012-12-11 17:42:02 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+/* nodejs:true */
+
+function DSN() {
+}
+
+module.exports = {
+    parse: function(dsn) {
+        var m = /^(mysql|pgsql):(.*)/.exec(dsn);
+        var rest;
+        var o = new DSN();
+        if (!m) return null;
+
+        o.protocol = m[1];
+
+        rest = m[2].split(/[:;]/);
+
+        rest.forEach(function(param) {
+            var m = /(.*)=(.*)/.exec(param);
+            
+            if (m[1].match(/^(host|port|dbname)$/)) {
+                o[m[1]] = m[2];
+            }
+            
+        });
+
+        return o;
+    }
+};
